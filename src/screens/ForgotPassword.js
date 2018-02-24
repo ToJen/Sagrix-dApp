d37a893e3b477a1001f44c0945aa1e9109017b10
Tomisin <tomisinjenrola@gmail.com>
2018-02-24 20:06:29 -0330

    Refactored screens and added new template ones

---
+++ src/screens/ForgotPassword.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class ForgotPasswordScreen extends Component {
+	render() {
+		return(
+      <View>
+        <Text>Forgot Password</Text>
+        <Text>skip for now, not priority</Text>
+      </View>
+		)
+	}
+}
