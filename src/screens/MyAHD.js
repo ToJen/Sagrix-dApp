ffafd4cee137f86711a9ae6a8566c180d44c780c
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2018-02-27 14:15:01 -0330

    Added drawer navigation

---
+++ src/screens/MyAHD.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Animated, Image} from 'react-native'
+import {DrawerNavigator} from 'react-navigation';
+
+//const Sliding_Drawer_Width = 350;
+export default class MyAHDScreen extends Component {
+	render() {
+		return(
+      <View>
+        <Text>MyAHD</Text>
+      </View>
+		)
+	}
+}
