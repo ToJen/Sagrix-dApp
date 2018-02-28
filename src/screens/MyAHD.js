08d4cd7885bd381d23d7fce5a150af88e8f406cf
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2018-02-27 23:26:01 -0330

    Minor bug fix in MyAHD

--- src/screens/MyAHD.js
+++ src/screens/MyAHD.js
@@ -10,15 +10,15 @@ export default class MyAHDScreen extends Component {
 	render() {
 
     const ahdQuestions = [
-                        'Medical treatment for the primary purposes of research', 
+                        'Medical treatment for the primary purposes of research',
                         'Sterilization that is not medically necessary for the protection of my health',
                         'The removal of tissue from my body while I am living for transplantation to another person',
                         'The removal of tissue from my body while I am living for the purpose of medical education or medical research'
                       ];
 
     const ahdCheckboxes = ahdQuestions.map((question) => {
-                                            return <CheckBox 
-                                                            key={Math.random} 
+                                            return <CheckBox
+                                                            key={Math.random}
                                                             label={question}
                                                             labelStyle={styles.questionStyle}
                                                             underLayColor='#e7ff6e'
@@ -27,14 +27,14 @@ export default class MyAHDScreen extends Component {
 
 		return(
       <View style={styles.container}>
-        <ScrollView>  
-          <Text style={styles.bannerText}>You've authorised <Text style={styles.span}>4 people</Text> to make decisions on your behalf</Text>
+        <ScrollView>
+          <Text style={styles.bannerText}>You have authorised <Text style={styles.span}>4 people</Text> to make decisions on your behalf</Text>
           <Text style={{textAlign:'justify'}}>{'\n'}I consent to:{'\n'}</Text>
           <View style={styles.questions}>
             {ahdCheckboxes}
           </View>
           <Text>{'\n'}</Text>
-          <Button 
+          <Button
             style={styles.update}
             color='#e7ff6e'
             title="UPDATE AHD"
@@ -42,7 +42,7 @@ export default class MyAHDScreen extends Component {
             onPress={() => this.props.navigation.navigate('MyAHDScreen')}
            />
           <Text>{'\n'}All physicians in your Circle of Care will be notified of any changes made.</Text>
-        </ScrollView>  
+        </ScrollView>
       </View>
 		)
 	}
