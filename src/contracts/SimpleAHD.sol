de58b1d81ae75c12022157b89c7539d5d513d901
Tomisin <tomisinjenrola@gmail.com>
2018-03-27 11:23:48 -0230

    Updated contract

--- src/contracts/SimpleAHD.sol
+++ src/contracts/SimpleAHD.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
 contract SimpleAHD {
 
@@ -25,55 +25,70 @@ contract SimpleAHD {
   function SimpleAHD() public {}
 
   function register(bytes32 _name) public returns(bool) {
-    require(!isRegistered(msg.sender));
+    if (isRegistered(msg.sender)) return false;
     patients[msg.sender].name = _name;
-    // patients[msg.sender].responses = _responses;
-    // patients[msg.sender].circle = _circle;
-    // patients[msg.sender].accessTimes = _acessTimes;
+    /*patients[msg.sender].responses = _responses;
+    patients[msg.sender].circle = _circle;
+    patients[msg.sender].accessTimes = _acessTimes;*/
     /*initializeMapping(_responses, 'responses');
     initializeMapping(_circle, 'circle');
     initializeMapping(_accessTimes, 'accessTimes');*/
     patients[msg.sender].id = patientIndex.push(msg.sender)-1;
-    PatientRegistered(msg.sender, patients[msg.sender].id);
+    emit PatientRegistered(msg.sender, patients[msg.sender].id);
     return true;
   }
 
   function isRegistered(address _patientAddress) public view returns(bool) {
-    require(patientIndex.length > 0);
+    if (patientIndex.length == 0) return false;
     return (patientIndex[patients[_patientAddress].id] == _patientAddress);
   }
 
-  function addToCircle(bytes32 _name) public {
-    require(patients[msg.sender].circle[_name] == true);
+  function addToCircle(bytes32 _name) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
+    if (patients[msg.sender].circle[_name] == true) return false;
     patients[msg.sender].circle[_name] = true;
-    AddedToCircle(msg.sender, _name);
+    emit AddedToCircle(msg.sender, _name);
+    return true;
   }
 
-  function removeFromCircle(bytes32 _name) public {
-    require(patients[msg.sender].circle[_name] == false);
+  function removeFromCircle(bytes32 _name) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
+    if (patients[msg.sender].circle[_name] == false) return false;
     patients[msg.sender].circle[_name] = false;
-    RemovedFromCircle(msg.sender, _name);
+    emit RemovedFromCircle(msg.sender, _name);
+    return true;
   }
 
-  function updateResponse(bytes32 _question, bool _answer) public {
+  function updateResponse(bytes32 _question, bool _answer) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
     patients[msg.sender].responses[_question] = _answer;
-    UpdatedResponse(msg.sender, _question, _answer);
+    emit UpdatedResponse(msg.sender, _question, _answer);
+    return true;
   }
 
-  function grantDataAccess(bytes32 _name, uint _duration) public {
+  function grantDataAccess(bytes32 _name, uint _duration) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
+    if (patients[msg.sender].accessTimes[_name] == _duration) return false;
     patients[msg.sender].accessTimes[_name] = _duration;
-    GrantedDataAccess(msg.sender, _name, _duration);
+    emit GrantedDataAccess(msg.sender, _name, _duration);
+    return true;
   }
 
-  function modifyDataAccess(bytes32 _name, uint _duration) public {
+  function modifyDataAccess(bytes32 _name, uint _duration) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
+    if (patients[msg.sender].accessTimes[_name] == 0) return false;
+    if (patients[msg.sender].accessTimes[_name] == _duration) return false;
     patients[msg.sender].accessTimes[_name] = _duration;
-    ModifiedDataAccess(msg.sender, _name, _duration);
+    emit ModifiedDataAccess(msg.sender, _name, _duration);
+    return true;
   }
 
-  function revokeDataAccess(bytes32 _name) public {
-    require(patients[msg.sender].accessTimes[_name] > 0);
+  function revokeDataAccess(bytes32 _name) public returns(bool) {
+    if (!isRegistered(msg.sender)) return false;
+    if (patients[msg.sender].accessTimes[_name] == 0) return false;
     patients[msg.sender].accessTimes[_name] = 0;
-    RevokedDataAccess(msg.sender, _name);
+    emit RevokedDataAccess(msg.sender, _name);
+    return true;
   }
 
   /*function initializeMapping(bytes32[] dataArray, string theMapping) private {
